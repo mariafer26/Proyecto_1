25dd5821c223ecb44845b02f69f4f53ae0ce615c
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-03-19 19:00:07 -0500

    Add files via upload

---
+++ PROYECTO1/src/EarthBeing/LivingBeing.java
@@ -0,0 +1,19 @@
+package EarthBeing;
+
+public abstract class LivingBeing {
+    public void born() {
+        System.out.println("Born...");
+    }
+    public void breathe() {
+        System.out.println("Breathing...");
+    }
+    public void grow() {
+        System.out.println("Growing...");
+    }
+    public void reproduce() {
+        System.out.println("Reproduce...");
+    }
+    public void die() {
+        System.out.println("Diying...");
+    }
+}
