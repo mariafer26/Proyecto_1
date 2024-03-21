c9a71518e3fb9fd7362817a3df408d58fec69608
mariafer26 <157810200+mariafer26@users.noreply.github.com>
2024-03-21 15:02:27 -0500

    Delete Proyecto_1/src directory

--- Proyecto_1/src/Principal/Earthbeing.java
+++
@@ -1,9 +0,0 @@
-package Principal;
-
-public interface Earthbeing {
-    public void born();
-    public void grow();
-    public void die();
-    public void eat();
-    public void fallinlove();
-}
--- Proyecto_1/src/Principal/LivingBeing.java
+++
@@ -1,8 +0,0 @@
-package Principal;
-
-public abstract class LivingBeing {
-    public abstract void born();
-    public abstract void grow();
-    public abstract void die();
-    public abstract void reproduce();
-}
