7acbf612c9c2f8b39fa7d28591d562676db0627f
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-03-21 23:13:40 -0500

    Update Dog.java

--- PROYECTO1/src/BeingOfEarth/Dog.java
+++ PROYECTO1/src/BeingOfEarth/Dog.java
@@ -1,20 +1,21 @@
-package BeingOfEarth;
+package BeingOfEarth; //Llamamos el paquete BeingOfEarth
 
-import EarthBeing.LivingBeing;
+import EarthBeing.LivingBeing; //Importamos la clase LivingBeing abstracta del paquete EarthBeing
 
-public class Dog extends LivingBeing{
-    private String name;
-    private int age;
+public class Dog extends LivingBeing{ //Creamos la clase Dog que extiende de la clase abstracta LivingBeing
+    private String name; //Declaramos el atributo name de tipo String
+    private int age; //Declaramos el atributo age de tipo int
 
-    public Dog(String name, int age) {
+    public Dog(String name, int age) {  //Creamos el constructor de la clase Dog con los atributos name y age
         this.name = name;
         this.age = age;
     }
 
-    public Dog() {
-
+    public Dog() {  //Creamos el constructor de la clase Dog sin atributos
+        super();
     }
 
+    //Creamos los métodos get y set para los atributos name y age
     public void born(){
         System.out.println("The dog is born");
     }
@@ -34,6 +35,7 @@ public class Dog extends LivingBeing{
         System.out.println("The dog is barking");
     }
 
+    //Creamos el metodo toString de la clase Dog que muestra los atributos de la clase Dog
     @Override
     public String toString() {
         return "Dog" +
@@ -42,3 +44,4 @@ public class Dog extends LivingBeing{
     }
 
 }
+
