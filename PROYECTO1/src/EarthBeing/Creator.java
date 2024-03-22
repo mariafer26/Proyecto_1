39696ac56323091195ff829c1d2aa65a845db6b0
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-03-21 23:14:46 -0500

    Update Creator.java

--- PROYECTO1/src/EarthBeing/Creator.java
+++ PROYECTO1/src/EarthBeing/Creator.java
@@ -1,6 +1,5 @@
-package EarthBeing;
+package EarthBeing; //LLamamos el paquete EarthBeing
 
-public interface Creator {
-    LivingBeing createLivingBeings();
-
+public interface Creator { //Creamos la interfaz Creator
+    LivingBeing createLivingBeings(); //Creamos el metodo createLivingBeings que retorna un objeto de tipo LivingBeing
     }
