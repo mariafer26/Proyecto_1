e73c4817d690b281cb25fb5c207b04efa8179373
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-03-21 23:14:16 -0500

    Update Human.java

--- PROYECTO1/src/BeingOfEarth/Human.java
+++ PROYECTO1/src/BeingOfEarth/Human.java
@@ -1,34 +1,32 @@
-package BeingOfEarth;
+package BeingOfEarth; //Llamamos el paquete BeingOfEarth
 
-import EarthBeing.EarthBeing;
-import EarthBeing.LivingBeing;
+import EarthBeing.EarthBeing;   //Importamos la interfaz EarthBeing del paquete EarthBeing
+import EarthBeing.LivingBeing;  //Importamos la clase LivingBeing abstracta del paquete EarthBeing
 
-public class Human extends LivingBeing implements EarthBeing {
+public class Human extends LivingBeing implements EarthBeing {  //Creamos la clase Human que extiende de la clase abstracta LivingBeing e implementa la interfaz EarthBeing
 
-    private String name;
-    private int age;
+    private String name;    //Declaramos el atributo name de tipo String
+    private int age;    //Declaramos el atributo age de tipo int
 
-    public Human(String name, int age) {
+    public Human(String name, int age) {    //Creamos el constructor de la clase Human con los atributos name y age
         this.name = name;
         this.age = age;
     }
 
+    //Getters y Setters de los atributos name y age
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
+    //Métodos de la interfaz EarthBeing
     @Override
     public void born() {
         super.born();
@@ -53,7 +51,7 @@ public class Human extends LivingBeing implements EarthBeing {
     public void die() {
         super.die();
     }
-
+    //Creamos el metodo toString de la clase Human que muestra los atributos de la clase Human
     @Override
     public String toString() {
         return "Human" +
