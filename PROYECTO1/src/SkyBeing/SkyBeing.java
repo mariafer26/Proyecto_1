e56582aa4b5cfbf4fab86bb344d784c940f2e3ef
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-03-21 23:17:43 -0500

    Update SkyBeing.java

--- PROYECTO1/src/SkyBeing/SkyBeing.java
+++ PROYECTO1/src/SkyBeing/SkyBeing.java
@@ -1,6 +1,7 @@
-package SkyBeing;
+package SkyBeing; //Llamamos el paquete SkyBeing
 
-public interface SkyBeing {
+public interface SkyBeing { //Creamos la interfaz SkyBeing
+    //Creamos los metodos fly y changedimension
     void fly();
     void changedimension();
 }
