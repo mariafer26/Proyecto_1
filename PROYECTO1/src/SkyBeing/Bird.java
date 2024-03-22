fb721442685997bce5b039e47b5ddc9a26417d8d
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-03-21 23:17:10 -0500

    Update Bird.java

--- PROYECTO1/src/SkyBeing/Bird.java
+++ PROYECTO1/src/SkyBeing/Bird.java
@@ -1,22 +1,21 @@
-package SkyBeing;
-import EarthBeing.LivingBeing;
+package SkyBeing;   // Llamamos el paquete SkyBeing
+import EarthBeing.LivingBeing; //Llamamos el paquete EarthBeing y la clase abstracta LivingBeing
 
-public class Bird extends LivingBeing implements SkyBeing{
-
+public class Bird extends LivingBeing implements SkyBeing{ //Creamos la clase Bird que extiende de la clase abstracta LivingBeing e implementa la interfaz SkyBeing
+    //Creamos los atributos de la clase Bird
     private String name;
     private String color;
     private int age;
-
+    //Creamos los constructores de la clase Bird
     public Bird(String name, String color, int age) {
         this.name = name;
         this.color = color;
         this.age = age;
     }
-
     public Bird(String name, int age) {
         super();
     }
-
+    //Creamos los metodos get y set de la clase Bird
     public String getName() {
         return name;
     }
@@ -40,7 +39,7 @@ public class Bird extends LivingBeing implements SkyBeing{
     public void setAge(int age) {
         this.age = age;
     }
-
+    //Creamos los metodos de la interfaz SkyBeing
     public void fly() {
         System.out.println("I'm flying");
     }
@@ -53,6 +52,10 @@ public class Bird extends LivingBeing implements SkyBeing{
     public void breathe(){
         System.out.println("The bird is breathing");
     }
+    public void die(){
+        System.out.println("The bird is dying");
+    }
+    //Creamos el metodo toString de la clase Bird que muestra los atributos de la clase Bird
     @Override
     public String toString() {
         return "Bird" +
