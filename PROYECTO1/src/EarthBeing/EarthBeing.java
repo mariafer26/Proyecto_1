faeb03b998605fb4f7ec8e668392c96c554d4982
Anna Sofía Giraldo Carvajal <157810727+AnnaSG27@users.noreply.github.com>
2024-03-21 23:15:15 -0500

    Update EarthBeing.java

--- PROYECTO1/src/EarthBeing/EarthBeing.java
+++ PROYECTO1/src/EarthBeing/EarthBeing.java
@@ -1,6 +1,7 @@
-package EarthBeing;
+package EarthBeing; //Llamamos el paquete EarthBeing
 
-public interface EarthBeing {
+public interface EarthBeing { //Creamos la interfaz EarthBeing
+    //Creamos los metodos que seran implementados en las clases que implementen esta interfaz
     void born();
     void grow();
     void reproduce();
